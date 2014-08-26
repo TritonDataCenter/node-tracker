dd972c95d971be06e386dc22420420cbd35e04e5
Orlando Vazquez <ovazquez@gmail.com>
2014-08-26 17:39:57 +0000

    TOOLS-607 I for one, welcome my new open source license

--- lib/monitor/dscripts/show_all_zone_up_down.d
+++ lib/monitor/dscripts/show_all_zone_up_down.d
@@ -1,5 +1,15 @@
 #pragma D option quiet
 
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * Copyright (c) 2014, Joyent, Inc.
+ */
+
 fbt::zone_status_set:entry
 {
   printf("{\"date\":\"%Y\",\"status\":\"%d\",\"name\":\"%s\"}\n",
--- lib/monitor/dscripts/zone_events.d
+++ lib/monitor/dscripts/zone_events.d
@@ -1,6 +1,16 @@
 #pragma D option quiet
 
 /*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * Copyright (c) 2014, Joyent, Inc.
+ */
+
+/*
  * Zone Transition Events
  */
 
